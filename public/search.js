0e66905f6c8b05531151ed66e039911085e21252
Mathias Buus <mathiasbuus@gmail.com>
2013-09-16 23:52:38 +0200

    updated templates to use the new indexing system

--- public/search.js
+++ public/search.js
@@ -1,12 +1,4 @@
 (function() {
-	var addClass = function(el, name) {
-		el.className = el.className.replace(name, '') + ' '+name;
-	};
-
-	var removeClass = function(el, name) {
-		el.className = el.className.replace(name, '');
-	};
-
 	var getURL = function(url, retries, callback) {
 		var xhr = new XMLHttpRequest();
 		var retry = function() {
@@ -22,39 +14,6 @@
 
 		xhr.open('GET', url);
 		xhr.send(null);
-	};
-
-	window.onindexing = function() {
-		var states = ['.','..','...',''];
-		var interval = setInterval(function() {
-			states.push(states.shift());
-			document.getElementById('progress').innerHTML = states[states.length-1];
-		}, 500);
-
-		var then = new Date();
-
-		var onprogressend = function(winner) {
-			document.getElementById('progress').innerHTML = '';
-			clearInterval(interval);
-			addClass(document.getElementById('index-progress'), 'hidden');
-			removeClass(document.getElementById('index-'+winner), 'hidden');
-		};
-
-		var onindexerror = function() {
-			document.cookie = 'username=; Expires=Mon, 01 Jan 1990 00:00:00 GMT';
-			document.getElementById('u').value = '';
-			getURL('/search?q='+window.QUERY+'&u=&partial=1', 0, function(err, modules) {
-				onprogressend('error');
-				document.getElementById('search').innerHTML = modules || '';
-			});
-		};
-
-		getURL('/search?q='+window.QUERY+'&u='+window.USERNAME+'&partial=1', 0, function(err, modules) {
-			if (err) return onindexerror();
-			onprogressend('complete');
-			document.getElementById('search').innerHTML = modules;
-			document.getElementById('time').innerHTML = Math.round((new Date().getTime() - then.getTime()) / 100) / 10;
-		});
 	};
 
 	var lastMarker;
