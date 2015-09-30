e4c212d8705c63290bfbc518e3b15166c4ada1e7
Mathias Buus <mathiasbuus@gmail.com>
2015-09-30 12:03:33 -0700

    use ScramSHA1

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 var mongojs = require('mongojs');
 var param = require('param');
-var db = mongojs(param('mongo'), ['modules', 'meta', 'users']);
+var db = mongojs(param('mongo'), ['modules', 'meta', 'users'], {authMechanism: 'ScramSHA1'});
 
 module.exports = db;
